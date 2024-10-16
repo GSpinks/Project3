08337ae4431fbad33670b8d8d2a81845ff8cf6ca
GSpinks <gts858@umsystem.edu>
2024-10-16 17:45:52 -0500

    Added Input for Time Series with input validation

--- Project3.py
+++ Project3.py
@@ -7,7 +7,16 @@ def getChart():
         if types == "1" or types == "2":
             return types
         else:
-            print("Invalid input. Please enter 1 or 2.")
+            print("Enter a 1 or 2 for chart type")
+
+def getTimeSeries():
+    while True:
+        dash = ("-"*56)
+        time = input(f"\nSelect the Time Series of the chart you want to Generate\n{dash}\n1. Intraday\n2. Daily\n3. Weekly\n4. Monthly\n\nEnter the time series option (1, 2, 3, 4): ")
+        if time == "1" or time == "2" or time == "3" or time == "4":
+            return time
+        else:
+            print("Enter a 1, 2, 3, or 4, for Time Series")
 
 
 def main():
@@ -18,6 +27,8 @@ def main():
     print(stock)
     chart = getChart()
     print(chart)
+    time = getTimeSeries()
+    print(time)
 
 if __name__ == "__main__":
     main()
